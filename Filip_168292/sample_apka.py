afed26e0b228cea6893ebb3a78046e8d8b56dd91
slendakowskif <wwx26500@student.warszawa.merito.pl>
2024-12-18 19:19:47 +0100

    Update sample_apka.py
    Dodanie pliku do właściwej lokalizacji

--- Filip_168292/sample_apka.py
+++ Filip_168292/sample_apka.py
@@ -1,1 +1,18 @@
+from flask import Flask, redirect
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, World! this is the main page! <h1>HELLO</h1>"
+
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}!"
+
+@app.route("/wsb")
+def wsb():
+    return redirect("https://www.merito.pl/warszawa/")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
